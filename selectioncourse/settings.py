4f8fb87e43dcf8d1f9180074f722e6b939c90f22
Monireh Bastami <monirehbastami@gmail.com>
2023-10-25 10:03:00 -0700

    create .env,Dockefile and update setting file

--- selectioncourse/settings.py
+++ selectioncourse/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@@j%_cnm74#$4)@1y&%g1zobxt4tt)y$f#hjc&)*pensp_xkut'
+SECRET_KEY = os.getenv('SECRET_KEY','MoMo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_MODE",False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'selectioncourse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.getenv("DB_NAME"),
+        #'USER': os.getenv("DB_USERNAME"),
+        #'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        #'PORT': os.getenv("DB_PORT")
     }
 }
 
