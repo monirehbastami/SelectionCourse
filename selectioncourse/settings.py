38ca365279f1c53fbe3a9272466ad134e57a356d
Monireh Bastami <monirehbastami@gmail.com>
2023-10-25 22:36:56 -0700

    feat requirements.txt

--- selectioncourse/settings.py
+++ selectioncourse/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'selectioncourse.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.getenv("DB_NAME"),
-        #'USER': os.getenv("DB_USERNAME"),
-        #'PASSWORD': os.getenv("DB_PASSWORD"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USERNAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        #'PORT': os.getenv("DB_PORT")
+        'PORT': os.getenv("DB_PORT")
     }
 }
 
